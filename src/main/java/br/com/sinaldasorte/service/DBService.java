0b1c928719734775876d21a133748c5fc0a4355f
Sant'Clear <santclear@gmail.com>
2018-02-12 02:25:41 -0200

    Instanciado nova conta no DBService, para teste

--- src/main/java/br/com/sinaldasorte/service/DBService.java
+++ src/main/java/br/com/sinaldasorte/service/DBService.java
@@ -6,14 +6,21 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import br.com.sinaldasorte.domain.Conta;
+
 @Service
 public class DBService {
 	
 	@Autowired
 	private BCryptPasswordEncoder encriptadorDeSenha;
 	
+	@Autowired
+	private ContaService contaService;
+	
 	public void instantiateTestDatabase() throws ParseException {
 		
+		Conta conta1 = new Conta(null, "sant@gmail.com", null, null, null, encriptadorDeSenha.encode("123"));
 		
+		contaService.insert(conta1);
 	}
 }
